201ad35863c98f4efc1f06fd63b391bf50075b04
Yana <yana23300@gmail.com>
2022-11-01 09:47:42 +0300

    introduced function

--- js/todo.js
+++ js/todo.js
@@ -1,4 +1,22 @@
+'use strict';
+
 // объявления переменынх //
 const field = document.querySelector('.field');
 const add = document.querySelector('.add');
 const list = document.querySelector('.list');
+
+// создаем функцию, создающую таски //
+function createTask(value) {
+    const task = document.createElement ("div");
+    task.textContent = value;
+    return task;
+}
+
+// создаем функцию, добавляющую таски //
+function addTask () {
+    if (field.classList.contains('field')) {
+        const newTask = createTask(field.value);
+        list.appendChild(newTask);
+        field.value = '';
+    }
+}
