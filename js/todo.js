bfdc2447839e38919b601cd22c5cba0d110dbd5c
Yana <yana23300@gmail.com>
2022-11-01 10:21:08 +0300

    completed function createTask

--- js/todo.js
+++ js/todo.js
@@ -2,17 +2,36 @@
 
 // объявления переменынх //
 const field = document.querySelector('.field');
-const add = document.querySelector('.add');
+const button = document.querySelector('.add');
 const list = document.querySelector('.list');
+const checkbox = document.querySelector('.checkbox')
+
+// eventLister //
+
+button.addEventListener('click', addTask) 
+
 
 // создаем функцию, создающую таски //
+
 function createTask(value) {
     const task = document.createElement ("div");
     task.textContent = value;
+    // добавляем класс для стилизации //
+    task.classList.add("new-task")
+    // добавляем чекбокс //
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.classList.add('status');
+    task.append(checkbox);
+    checkbox.addEventListener('click', completeTask);
+
     return task;
 }
 
+
+
 // создаем функцию, добавляющую таски //
+
 function addTask () {
     if (field.classList.contains('field')) {
         const newTask = createTask(field.value);
