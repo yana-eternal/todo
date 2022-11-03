1f7d4a05051299b2f8f2f45e74c2549f324e6d83
Yana <yana23300@gmail.com>
2022-11-03 16:47:19 +0300

    character limit

--- js/todo.js
+++ js/todo.js
@@ -7,12 +7,13 @@ const list = document.querySelector(".list");
 const checkbox = document.querySelector(".checkbox");
 const form = document.querySelector(".todo-form");
 const priority = document.querySelector(".todo-priority");
+const warning = document.querySelector(".warning-none");
 
 // база //
-// создаем функцию, создающую таски //
+
+// функция, создающая таски //
 function createTask(value) {
   const task = document.createElement("div");
-  // добавляем класс для стилизации //
   task.classList.add("new-task");
   const text = document.createElement("p");
   text.classList.add("text");
@@ -24,7 +25,7 @@ function createTask(value) {
   task.append(checkbox);
   task.append(text);
   checkbox.addEventListener("click", completeTask);
-  // добавляем пин //
+  // пин //
   const pin = document.createElement("button");
   pin.classList.add("pin");
   task.append(pin);
@@ -35,9 +36,9 @@ function createTask(value) {
   deleteButton.addEventListener("click", deleteTask);
   return task;
 }
-// создаем функцию, добавляющую таски //
-// нужно для required и переключения класса задачи//
-form.onsubmit = addTask;
+
+// функция, добавляющая таски //
+form.onsubmit = addTask; // onsubmit нужно для required и переключения класса задачи//
 
 function addTask(event) {
   event.preventDefault();
@@ -50,29 +51,26 @@ function addTask(event) {
   list.appendChild(newTask);
   field.value = "";
 }
-// complete task //
+
+// функции, выплняющие и удаляющие таски //
 function completeTask(evt) {
-  // короткая запись //
-  const { target } = evt;
-  //   const target = evt.target;
+  const { target } = evt; // короткая запись const target = evt.target //
   if (target.checked) {
     target.parentElement.classList.add("success");
   } else {
     target.parentElement.classList.remove("success");
   }
 }
-// удаление задачи //
 function deleteTask(event) {
   const target = event.target;
   const targetBox = target.parentElement;
   targetBox.remove();
 }
 
-// обработчики //
-// eventLister //
-button.addEventListener("submit", addTask);
+// обработчики  функций//
+button.addEventListener("submit", addTask); // добавляет задачи при отправке
 
-// класc задачи //
+// меняет класc задачи //
 priority.addEventListener("click", toggle);
 function toggle() {
   priority.classList.toggle("is-important");
@@ -82,3 +80,16 @@ function toggle() {
     priority.textContent = "Обычная задача";
   }
 }
+
+// огранчение колво символов
+field.oninput = function () {
+  if (field.value.length > 80) {
+    button.classList.add("disabled-btn");
+    button.disabled = true;
+    warning.classList.add("warning");
+  } else {
+    button.classList.remove("disabled-btn");
+    button.disabled = false;
+    warning.classList.remove("warning");
+  }
+};
